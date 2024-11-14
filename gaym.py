f43de3710a4c2b999e9c4b1454ad6a07718c2f9a
MrAnt22 <makslipovyi@gmail.com>
2024-11-14 21:09:38 +0200

    realupdatered

--- gaym.py
+++ gaym.py
@@ -1,20 +1,29 @@
 from tkinter import *
 import random
+import pygame
+
 
 root = Tk()
 
 root.title("Ратмира ігрове вікно")
-root.minsize(240, 255)
 root.geometry("1000x800")
 root.configure(bg="#D5BA33")
 root.maxsize(1000, 800)
+
+pygame.mixer.init()
+music1 = pygame.mixer.Sound("C:/Users/maksl/Desktop/redfolder/music/ithas.mp3")
+music1.set_volume(0.1)
+music1.play()
+
+bg1 = PhotoImage(file="C:/Users/maksl/Desktop/redfolder/lordaeron.png")
 
 art = PhotoImage(file="C:/Users/maksl/Desktop/redfolder/arthas.png", width=200, height=228)
 krl = PhotoImage(file="C:/Users/maksl/Desktop/redfolder/snake.png", width=103, height=200)
 
 w, h = 1000, 720
-orig_canva = Canvas(root, height=h, width=w, bg="black")
-orig_canva.pack()
+orig_canva = Canvas(root, height=h, width=w)
+orig_canva.pack(expand=True, fill="both")
+orig_canva.create_image(0,0, image=bg1,anchor = "nw")
 
 class Arthas:
     def __init__(self, canvas):
@@ -45,33 +54,53 @@ class Snake:
         self.canvas.coords(self.image_id, self.x, self.y)
         self.canvas.coords(self.hitbox, self.x - 10, self.y - 40, self.x + 70, self.y + 70)
 
+
 arthas = Arthas(orig_canva)
 snake = Snake(orig_canva)
 
-score = 0
 
+score = 0
 def score_upd():
     global score
     score += 1
     scr.config(text = "Score: {}".format(score))
-
+    snake_attack()
 scr = Label(root,text="Score: 0", bg="white", fg="red",width=100,height=30, font="aerial 14")
 scr.pack()
 
+
+is_stunned = False
+stun_message_id = None
+def snake_attack():
+    global is_stunned, stun_message_id
+    if score % 5 == 0 and score != 0:
+        if not is_stunned:
+            is_stunned = True
+            stun_message_id = orig_canva.create_text(w // 2, h // 2, text="Arthas is stunned by coward Snake! Press the left mouse button to keep fighting!", font="Arial 14", fill="red")
+            root.bind("<Button-1>", release_stun)
+def release_stun(event):
+    global is_stunned, stun_message_id
+    is_stunned = False
+    if stun_message_id:
+        orig_canva.delete(stun_message_id)
+        stun_message_id = None
+    root.unbind("<Button-1>")
+
+
 def left(event):
-    if arthas.x > 0:
+    if arthas.x > 0 and not is_stunned:
         arthas.move(-10, 0)
         hit()
 def right(event):
-    if arthas.x < w:
+    if arthas.x < w and not is_stunned:
         arthas.move(10, 0)
         hit()
 def up(event):
-    if arthas.y > 0:
+    if arthas.y > 0 and not is_stunned:
         arthas.move(0, -10)
         hit()
 def down(event):
-    if arthas.y < h:
+    if arthas.y < h and not is_stunned:
         arthas.move(0, 10)
         hit()
 def hit():
@@ -84,6 +113,7 @@ def hit():
         print("nice!")
         snake.respawn()
         score_upd()
+
 
 root.bind("<Left>", left)
 root.bind("<Right>", right)
